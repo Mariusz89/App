5838993e9c193b6845089904da85756561ab7c89
Mariusz <mariusz.kkk@poczta.fm>
2020-04-03 00:29:53 +0200

    Added middleware bodyParser, morgan, cors. Created file .env and added variables like PORT, NODE_ENV, CLIENT_URL

--- api/server.js
+++ api/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 
 const authRoutes = require('./routes/auth');
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(cors({
+        origin: `http://localhost:3000`
+    }));
+}
+
 app.use('/api', authRoutes);
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`connect on the port ${port}`);
+    console.log(`connect on the port ${port} - ${process.env.NODE_ENV}`);
 });
