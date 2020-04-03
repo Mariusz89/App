a4f07c416876aa9a37cf6196fdae424ef5d9ae42
Mariusz <mariusz.kkk@poczta.fm>
2020-04-03 23:37:54 +0200

    Created validation for user signup: name, email, password. Added handle errors

---
+++ api/validators/auth.js
@@ -0,0 +1,31 @@
+const {check} = require('express-validator');
+
+exports.userSignupValidator = [
+	check('name')
+		.trim()
+		.not()
+		.isEmpty()
+		.withMessage('Name is required'),
+
+	check('email')
+		.trim()
+		.not()
+		.isEmpty()
+		.withMessage('Email is required'),
+
+	check('email')
+		.trim()
+		.isEmail()
+		.withMessage('Email is invalid'),
+
+	check('password')
+		.trim()
+		.not()
+		.isEmpty()
+		.withMessage('Password is required'),
+
+	check('password')
+		.trim()
+		.isLength({min: 8})
+		.withMessage('Password must be at least 8 characters long')
+];
