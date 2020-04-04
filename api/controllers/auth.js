03b0fc0cb50d6eafdf9a7ab3b4482cc7546b44d5
Mariusz <mariusz.kkk@poczta.fm>
2020-04-04 23:19:52 +0200

    Saves new user to database

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,37 @@
+const User = require('../models/user');
+
+
 exports.signup =  (req, res) => {
-    console.log(req.body);
-    
+    /*console.log(req.body);
+
     res.json({
         data: 'You hit signup endpoint !!!'
+    });*/
+
+    const {name, email, password} = req.body;
+    console.log(email);
+
+    User.findOne({email}).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: "Email is taken"
+            })
+        }
+    })
+
+    let newUser = new User({name, email, password});
+
+    newUser.save((err, success) => {
+        console.log("Signup user", err);
+
+        if(err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+
+        res.json({
+            message: "New user signup. Please sign in"
+        });
     });
-}
+};
