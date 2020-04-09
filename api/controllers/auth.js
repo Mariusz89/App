6ce3c3521313c7f070927899305e7b40c03632ba
Mariusz <mariusz.kkk@poczta.fm>
2020-04-09 22:08:05 +0200

    Created route /account-activation and saved user to database

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,37 +1,98 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED;
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 
 exports.signup =  (req, res) => {
-    /*console.log(req.body);
+	const {name, email, password} = req.body;
 
-    res.json({
-        data: 'You hit signup endpoint !!!'
-    });*/
+	User.findOne({email}).exec((err, user) => {
+		if (user) {
+			return res.status(400).json({
+				error: "Email is taken"
+			})
+		}
 
-    const {name, email, password} = req.body;
-    console.log(email);
+		const token = jwt.sign({name, email, password}, process.env.JWT_ACCOUNT_ACTIVATION, {expiresIn: '10m'});
+		console.log('token: ' + token)
 
-    User.findOne({email}).exec((err, user) => {
-        if (user) {
-            return res.status(400).json({
-                error: "Email is taken"
-            })
-        }
-    })
+		const smtpTransport = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: process.env.MAIL_USER,
+				pass: process.env.MAIL_PASS
+			}
+		});
 
-    let newUser = new User({name, email, password});
+		console.log(process.env.EMAIL_FROM);
+		
+		const mailOptions = {
+			from: process.env.EMAIL_FROM,
+			to: email,
+			subject: `Account activation link`,
+			text: "Please use the following link to activate your account",
+			html: `
+				<b>Please use the following link to activate your account</b>
+				<p>${process.env.CLIENT_URL}/auth/activate/${token}</p>	
+				<hr />
+				<p>This email may contain sensitive information</p>
+				<p>${process.env.CLIENT_URL}</p>
+			`
+		}
 
-    newUser.save((err, success) => {
-        console.log("Signup user", err);
+		smtpTransport.sendMail(mailOptions)
+			.then(sent => {
+				console.log('asasasa ' + sent.messageId);
 
-        if(err) {
-            return res.status(400).json({
-                error: err
-            });
-        }
+				return res.json({
+					message: `Email has been sent to ${email}. Follow the instruction to activate your account.`
+				})	
+			})
+			.catch(err => {			
+				return res.json({
+					message: err
+				})
+			})
 
-        res.json({
-            message: "New user signup. Please sign in"
-        });
-    });
+		smtpTransport.close();
+	});
 };
+
+exports.accountActivation = (req, res) => {
+	const {token} = req.body;
+
+	if(token) {
+		jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(err, decoded) {
+			if(err) {
+				console.log('JWT VERIFY IN ACCOUNT ACTIVATION ERROR', err);
+
+				return res.status(401).json({
+					error: 'Expired link. Signup again'
+				})
+			}
+
+			const {name, email, password} = jwt.decode(token);
+
+			const user = new User ({name, email, password});
+
+			user.save((err, user) => {
+				if(err) {
+					console.log('SAVE USER IN ACCOUNT ACTIVATION ERROR', err);
+
+					return res.status(401).json({
+						error: 'Error saving user in database. Try signup again'
+					});
+				}
+
+				return res.json({
+					message: 'Signup success. Please signup'
+				});
+			});
+		});
+	} else {
+			return res.json({
+				message: 'Something went wrong. Try again'
+			})
+	}
+}
