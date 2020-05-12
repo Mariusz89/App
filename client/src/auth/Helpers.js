c898e36a7327e2e97abb5151f1d0cd34f5a12c6d
Mariusz <mariusz.kkk@poczta.fm>
2020-05-12 23:07:37 +0200

    Created new file helpers. Added new functions: setCookie, removeCookie, getCookie, setLocalStorage, isAuth

---
+++ client/src/auth/Helpers.js
@@ -0,0 +1,57 @@
+import cookie from 'js-cookie';
+
+export const setCookie = (key, value) => {
+	if(window !== 'undefined') {
+		cookie.set(key, value, {
+			expires: 1
+		});
+	}
+};
+
+export const removeCookie = key => {
+	if(window !== 'undefined') {
+		cookie.remove(key, {
+			expires: 1
+		});
+	}
+};
+
+export const getCookie = key => {
+	if(window !== 'undefined') {
+		return cookie.get(key);
+	}
+};
+
+export const setLocalStorage = (key, value) => {
+	if(window !== 'undefined') {
+		localStorage.setItem(key, JSON.stringify(value));
+	}
+};
+
+export const isAuth = () => {
+	if(window !== 'undefined') {
+		const cookieChecked = getCookie('token');
+
+		if(cookieChecked) {
+			if(localStorage.getItem('user')) {
+				return JSON.parse(localStorage.getItem('user'));
+			} else {
+					return false;
+			}
+		}
+	}
+};
+
+export const removeLocalStorage = key => {
+	if(window !== 'undefined') {
+		localStorage.removeItem(key);
+	}
+};
+
+export const authenticate = (res, next) => {
+	console.log('Authenticate:', response);
+
+	setCookie('token', res.data.token);
+	setLocalStorage('user', res.data.user);
+	next();
+};
